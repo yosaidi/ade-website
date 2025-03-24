086c134eaaf33ad810dd6ba5233cd562f0ccface
yosaidi <younes.saidi.23@ump.ac.ma>
2025-03-24 23:42:58 +0000

    mad version

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,8 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navbar = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", function () {
+        navbar.classList.toggle("open");
+    });
 });
